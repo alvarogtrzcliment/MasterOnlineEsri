2f639fd4d97ffff93d3be74fd0c48df809a1531e
Álvaro Gutiérrez <124072319+alvarogtrzcliment@users.noreply.github.com>
2025-01-07 17:40:43 +0100

    JS

---
+++ JS/Ejercicio 2/main.js
@@ -0,0 +1,10 @@
+
+const cumpleanos = {
+    dia: 22,
+    mes: 'Febrero'
+}
+
+console.log('Mi cumpleaños es el ' + cumpleanos.dia + ' de ' + cumpleanos.mes)
+
+console.log(`Mi cumpleaños es el ${cumpleanos.dia} de ${cumpleanos.mes}`)
+
